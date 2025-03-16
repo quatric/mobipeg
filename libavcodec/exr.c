quatric/mobipeg

commit 63d2d8373b4cc19d53be652eedbfa9714636bb26

avcodec/exr: do not output 32bit floats when a file stores 16bit floats
exr should not do a internal pixel format convert

Sponsored-by: Sovereign Tech Fund
Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

libavcodec/exr.c
@@ -1185,7 +1185,7 @@ static int decode_block(AVCodecContext *avctx, void *tdata,
     int line, col = 0;
     uint64_t tile_x, tile_y, tile_level_x, tile_level_y;
     const uint8_t *src;
-    int step = s->desc->flags & AV_PIX_FMT_FLAG_FLOAT ? 4 : 2 * s->desc->nb_components;
+    int step = s->desc->comp[0].step;
     int bxmin = 0, axmax = 0, window_xoffset = 0;
     int window_xmin, window_xmax, window_ymin, window_ymax;
     int data_xoffset, data_yoffset, data_window_offset, xsize, ysize;
@@ -1365,7 +1365,7 @@ static int decode_block(AVCodecContext *avctx, void *tdata,
      if (s->channel_offsets[3] >= 0)
         channel_buffer[3] = src + (td->xsize * s->channel_offsets[3]) + data_window_offset;
 
-    if (s->desc->flags & AV_PIX_FMT_FLAG_FLOAT) {
+    if (s->desc->flags & AV_PIX_FMT_FLAG_PLANAR || s->desc->nb_components == 1 ) {
         /* todo: change this when a floating point pixel format with luma with alpha is implemented */
         int channel_count = s->channel_offsets[3] >= 0 ? 4 : rgb_channel_count;
         if (s->is_luma) {
@@ -1379,19 +1379,20 @@ static int decode_block(AVCodecContext *avctx, void *tdata,
 
             for (i = 0; i < ysize; i++, ptr += p->linesize[plane]) {
                 const uint8_t *src;
-                union av_intfloat32 *ptr_x;
-
-                src = channel_buffer[c];
-                ptr_x = (union av_intfloat32 *)ptr;
-
-                // Zero out the start if xmin is not 0
-                memset(ptr_x, 0, bxmin);
-                ptr_x += window_xoffset;
 
                 if (s->pixel_type == EXR_FLOAT ||
                     s->compression == EXR_DWAA ||
                     s->compression == EXR_DWAB) {
                     // 32-bit
+                    union av_intfloat32 *ptr_x;
+
+                    src = channel_buffer[c];
+                    ptr_x = (union av_intfloat32 *)ptr;
+
+                    // Zero out the start if xmin is not 0
+                    memset(ptr_x, 0, bxmin);
+                    ptr_x += window_xoffset;
+
                     union av_intfloat32 t;
                     if (trc_func && c < 3) {
                         for (x = 0; x < xsize; x++) {
@@ -1412,22 +1413,22 @@ static int decode_block(AVCodecContext *avctx, void *tdata,
                             *ptr_x++ = t;
                         }
                     }
+                    memset(ptr_x, 0, axmax);
                 } else if (s->pixel_type == EXR_HALF) {
+                    src = channel_buffer[c];
+
+                    // Zero out the start if xmin is not 0
+                    memset(ptr, 0, bxmin);
+
                     // 16-bit
-                    if (c < 3 || !trc_func) {
-                        for (x = 0; x < xsize; x++) {
-                            *ptr_x++ = s->gamma_table[bytestream_get_le16(&src)];
-                        }
-                    } else {
-                        for (x = 0; x < xsize; x++) {
-                            ptr_x[0].i = half2float(bytestream_get_le16(&src), &s->h2f_tables);
-                            ptr_x++;
-                        }
+                    for (x = window_xoffset; x < xsize + window_xoffset; x++) {
+                        int v = bytestream_get_le16(&src);
+                        AV_WN16(ptr + x * sizeof(uint16_t), v);
                     }
+                    memset(ptr + x * sizeof(uint16_t), 0, axmax);
                 }
 
                 // Zero out the end if xmax+1 is not w
-                memset(ptr_x, 0, axmax);
                 channel_buffer[c] += td->channel_line_size;
             }
         }
@@ -2053,8 +2054,25 @@ static int decode_frame(AVCodecContext *avctx, AVFrame *picture,
     }
 
     switch (s->pixel_type) {
-    case EXR_FLOAT:
     case EXR_HALF:
+        if (!(s->compression == EXR_DWAA || s->compression == EXR_DWAB)) {
+            if (s->channel_offsets[3] >= 0) {
+                if (!s->is_luma) {
+                    avctx->pix_fmt = AV_PIX_FMT_GBRAPF16;
+                } else {
+                    /* todo: change this when a floating point pixel format with luma with alpha is implemented */
+                    avctx->pix_fmt = AV_PIX_FMT_GBRAPF16;
+                }
+            } else {
+                if (!s->is_luma) {
+                    avctx->pix_fmt = AV_PIX_FMT_GBRPF16;
+                } else {
+                    avctx->pix_fmt = AV_PIX_FMT_GRAYF16;
+                }
+            }
+            break;
+        }
+    case EXR_FLOAT:
         if (s->channel_offsets[3] >= 0) {
             if (!s->is_luma) {
                 avctx->pix_fmt = AV_PIX_FMT_GBRAPF32;
@@ -2139,13 +2157,12 @@ static int decode_frame(AVCodecContext *avctx, AVFrame *picture,
     if (!s->desc)
         return AVERROR_INVALIDDATA;
 
-    if (s->desc->flags & AV_PIX_FMT_FLAG_FLOAT) {
+    if (s->desc->flags & AV_PIX_FMT_FLAG_PLANAR) {
         planes           = s->desc->nb_components;
-        out_line_size    = avctx->width * 4;
     } else {
         planes           = 1;
-        out_line_size    = avctx->width * 2 * s->desc->nb_components;
     }
+    out_line_size    = avctx->width * s->desc->comp[0].step;
 
     if (s->is_tile) {
         nb_blocks = ((s->xdelta + s->tile_attr.xSize - 1) / s->tile_attr.xSize) *

tests/ref/fate/exr-rgb-b44a-half-negative-4x4
@@ -3,4 +3,4 @@
 #codec_id 0: rawvideo
 #dimensions 0: 4x4
 #sar 0: 1/1
-0,          0,          0,        1,      192, 0x5b8e39c0
+0,          0,          0,        1,      192, 0x2b0438a0

tests/ref/fate/exr-rgb-scanline-b44-half-float-12x8-l1
@@ -3,4 +3,4 @@
 #codec_id 0: rawvideo
 #dimensions 0: 12x8
 #sar 0: 1/1
-0,          0,          0,        1,     1152, 0x577d5150
+0,          0,          0,        1,     1152, 0xa25c30dd

tests/ref/fate/exr-rgb-scanline-half-b44-12x8
@@ -3,4 +3,4 @@
 #codec_id 0: rawvideo
 #dimensions 0: 12x8
 #sar 0: 1/1
-0,          0,          0,        1,     1152, 0xe84d5b9d
+0,          0,          0,        1,     1152, 0xdb43ec2d

tests/ref/fate/exr-rgb-scanline-half-b44-13x9
@@ -3,4 +3,4 @@
 #codec_id 0: rawvideo
 #dimensions 0: 13x9
 #sar 0: 1/1
-0,          0,          0,        1,     1404, 0x252cc156
+0,          0,          0,        1,     1404, 0x108f51a5

tests/ref/fate/exr-rgb-scanline-half-piz-bw
@@ -3,4 +3,4 @@
 #codec_id 0: rawvideo
 #dimensions 0: 34x27
 #sar 0: 1/1
-0,          0,          0,        1,    11016, 0x1644e1f9
+0,          0,          0,        1,    11016, 0xa40e4a99

tests/ref/fate/exr-rgb-scanline-half-piz-color
@@ -3,4 +3,4 @@
 #codec_id 0: rawvideo
 #dimensions 0: 34x40
 #sar 0: 1/1
-0,          0,          0,        1,    16320, 0xc40939ad
+0,          0,          0,        1,    16320, 0xcb1cce31

tests/ref/fate/exr-rgb-scanline-half-piz-dw-large
@@ -3,4 +3,4 @@
 #codec_id 0: rawvideo
 #dimensions 0: 50x50
 #sar 0: 1/1
-0,          0,          0,        1,    30000, 0xb329ee9c
+0,          0,          0,        1,    30000, 0xef01f54f

commit 1d353ae7da4c0979d449e1b0176813f09dce94ae

avcodec/exrdec: restore applying color transfer characteristics for float16 samples
Regression since 63d2d8373b4cc19d53be652eedbfa9714636bb26.
This change also reverts commit 13d35607229cb83a1864ca8f0f01c0f926d41682.

Signed-off-by: James Almer <jamrial@gmail.com>

libavcodec/exr.c
@@ -42,6 +42,7 @@
 #include "libavutil/avstring.h"
 #include "libavutil/mem.h"
 #include "libavutil/opt.h"
+#include "libavutil/float2half.h"
 #include "libavutil/half2float.h"
 
 #include "avcodec.h"
@@ -193,7 +194,10 @@ typedef struct EXRContext {
 
     uint8_t *offset_table;
 
+    uint16_t gamma_table[65536];
+
     Half2FloatTables h2f_tables;
+    Float2HalfTables f2h_tables;
 } EXRContext;
 
 static int zip_uncompress(const EXRContext *s, const uint8_t *src, int compressed_size,
@@ -1401,8 +1405,13 @@ static int decode_block(AVCodecContext *avctx, void *tdata,
                     }
                 } else if (s->pixel_type == EXR_HALF) {
                     // 16-bit
-                    for (int x = 0; x < xsize; x++, ptr_x += step)
-                        AV_WN16A(ptr_x, bytestream_get_le16(&src));
+                    if (one_gamma != 1.f || (trc_func && (!c || (c < 3 && s->desc->flags & AV_PIX_FMT_FLAG_PLANAR)))) {
+                        for (int x = 0; x < xsize; x++, ptr_x += step)
+                            AV_WN16A(ptr_x, s->gamma_table[bytestream_get_le16(&src)]);
+                    } else {
+                        for (int x = 0; x < xsize; x++, ptr_x += step)
+                            AV_WN16A(ptr_x, bytestream_get_le16(&src));
+                    }
                 }
 
                 // Zero out the end if xmax+1 is not w
@@ -2212,8 +2221,13 @@ static int decode_frame(AVCodecContext *avctx, AVFrame *picture,
 static av_cold int decode_init(AVCodecContext *avctx)
 {
     EXRContext *s = avctx->priv_data;
+    uint32_t i;
+    union av_intfloat32 t;
+    float one_gamma = 1.0f / s->gamma;
+    av_csp_trc_function trc_func = NULL;
 
     ff_init_half2float_tables(&s->h2f_tables);
+    ff_init_float2half_tables(&s->f2h_tables);
 
     s->avctx              = avctx;
 
@@ -2222,6 +2236,26 @@ static av_cold int decode_init(AVCodecContext *avctx)
 #if HAVE_BIGENDIAN
     ff_bswapdsp_init(&s->bbdsp);
 #endif
+
+    trc_func = av_csp_trc_func_from_id(s->apply_trc_type);
+    if (trc_func) {
+        for (i = 0; i < 65536; ++i) {
+            t.i = half2float(i, &s->h2f_tables);
+            t.f = trc_func(t.f);
+            s->gamma_table[i] = float2half(av_float2int(t.f), &s->f2h_tables);
+        }
+    } else if (one_gamma != 1.0f) {
+        for (i = 0; i < 65536; ++i) {
+            t.i = half2float(i, &s->h2f_tables);
+            /* If negative value we reuse half value */
+            if (t.f <= 0.0f) {
+                s->gamma_table[i] = i;
+            } else {
+                t.f = powf(t.f, one_gamma);
+                s->gamma_table[i] = float2half(t.i, &s->f2h_tables);
+            }
+        }
+    }
 
     // allocate thread data, used for non EXR_RAW compression types
     s->thread_data = av_calloc(avctx->thread_count, sizeof(*s->thread_data));