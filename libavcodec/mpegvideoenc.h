mkver/FFmpeg

avcodec/mpegvideo: Move last-pic information to MPVMainEncContext
last_pict_type, last_non_b_pict_type and last_lambda_for
are only used by the encoder's main thread.

Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

libavcodec/mpegvideoenc.h
@@ -58,6 +58,10 @@ typedef struct MPVMainEncContext {
      * a delay */
     int64_t reordered_pts;
 
+    int last_pict_type; //FIXME remove
+    int last_non_b_pict_type;   ///< used for MPEG-4 gmc B-frames & ratecontrol
+    int last_lambda_for[5];     ///< last lambda for a specific pict type
+
     /* bit rate control */
     int64_t total_bits;
     int frame_bits;                ///< bits used for the current frame

libavcodec/snowenc.c
@@ -1858,7 +1858,7 @@ redo_frame:
             return -1;
     if(avctx->flags&AV_CODEC_FLAG_PASS1)
         ff_write_pass1_stats(&s->m);
-    mpv->last_pict_type = mpv->pict_type;
+    s->m.last_pict_type = mpv->pict_type;
 
     emms_c();
 

quatric/mobipeg

libavcodec/mpegvideoenc.h
@@ -96,6 +96,9 @@ typedef struct MPVMainEncContext {
     int stuffing_bits;             ///< bits used for stuffing
     int next_lambda;               ///< next lambda used for retrying to encode a frame
     int fixed_qscale;              ///< fixed qscale if non zero
+    int last_lambda_for[5];        ///< last lambda for a specific pict type
+    int last_pict_type;            //FIXME removes
+    int last_non_b_pict_type;      ///< used for MPEG-4 gmc B-frames & ratecontrol
     RateControlContext rc_context; ///< contains stuff only accessed in ratecontrol.c
 
     int64_t mb_var_sum;            ///< sum of MB variance for current frame

libavcodec/snowenc.c
@@ -2058,7 +2058,7 @@ redo_frame:
     }
     if(avctx->flags&AV_CODEC_FLAG_PASS1)
         ff_write_pass1_stats(&enc->m);
-    mpv->last_pict_type = mpv->pict_type;
+    enc->m.last_pict_type = mpv->pict_type;
 
     emms_c();
 