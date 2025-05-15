TINCHLIK/ffmpeg-android-build

commit a1b2baa3ef70da273bdfdcf376fd060d691fedaa

avformat/imf_cpl: Check the right variable
Fixes Coverity issue #1512407.

Reviewed-by: Pierre-Anthony Lemieux <pal@sandflow.com>
Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

libavformat/imf_cpl.c
@@ -625,7 +625,7 @@ static int fill_virtual_tracks(xmlNodePtr cpl_element, FFIMFCPL *cpl)
         av_log(NULL, AV_LOG_DEBUG, "Processing IMF CPL Segment\n");
 
         sequence_list_elem = ff_imf_xml_get_child_element_by_name(segment_elem, "SequenceList");
-        if (!segment_list_elem)
+        if (!sequence_list_elem)
             continue;
 
         sequence_elem = xmlFirstElementChild(sequence_list_elem);

quatric/mobipeg

commit 22a76f557a8b117da5b77e433a41b83722ca804d

avformat/imf_cpl: do not continue looping forever
Fixes: infinite loop
Fixes: 401658595/clusterfuzz-testcase-minimized-ffmpeg_DEMUXER_fuzzer-5756875014733824
Regression since: a1b2baa3ef70da273bdfdcf376fd060d691fedaa

Found-by: continuous fuzzing process https://github.com/google/oss-fuzz/tree/master/projects/ffmpeg
Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

libavformat/imf_cpl.c
@@ -709,8 +709,7 @@ static int fill_virtual_tracks(void *log_ctx, xmlNodePtr cpl_element, FFIMFCPL *
         av_log(log_ctx, AV_LOG_DEBUG, "Processing IMF CPL Segment\n");
 
         sequence_list_elem = ff_imf_xml_get_child_element_by_name(segment_elem, "SequenceList");
-        if (!sequence_list_elem)
-            continue;
+        if (sequence_list_elem) {
 
         sequence_elem = xmlFirstElementChild(sequence_list_elem);
         while (sequence_elem) {
@@ -734,6 +733,7 @@ static int fill_virtual_tracks(void *log_ctx, xmlNodePtr cpl_element, FFIMFCPL *
                 return ret;
 
             sequence_elem = xmlNextElementSibling(sequence_elem);
+        }
         }
 
         segment_elem = xmlNextElementSibling(segment_elem);