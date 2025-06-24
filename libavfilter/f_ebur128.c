quatric/mobipeg

commit 681eeed356cbe3a6c15b18e6ecac71bbba40fbf0

avfilter/f_ebur128: move peak detection to reusable DSP function
True peak and sample peak share almost the same logic. Define this logic in
a separate function for reusability, and so we can write SIMD versions.

libavfilter/f_ebur128.c
@@ -580,6 +580,8 @@ static av_cold int init(AVFilterContext *ctx)
     av_log(ctx, AV_LOG_VERBOSE, "EBU +%d scale\n", ebur128->meter);
 
     ebur128->dsp.filter_channels = ff_ebur128_filter_channels_c;
+    ebur128->dsp.find_peak = ff_ebur128_find_peak_c;
+
 #if ARCH_X86
     ff_ebur128_init_x86(&ebur128->dsp);
 #endif
@@ -650,6 +652,23 @@ void ff_ebur128_filter_channels_c(const EBUR128DSPContext *dsp,
     }
 }
 
+double ff_ebur128_find_peak_c(double *restrict ch_peaks, const int nb_channels,
+                              const double *samples, const int nb_samples)
+{
+    double maxpeak = 0.0;
+    for (int ch = 0; ch < nb_channels; ch++) {
+        double ch_peak = ch_peaks[ch];
+        for (int i = 0; i < nb_samples; i++) {
+            const double sample = fabs(samples[i * nb_channels]);
+            ch_peak = FFMAX(ch_peak, sample);
+        }
+        maxpeak = FFMAX(maxpeak, ch_peak);
+        ch_peaks[ch] = ch_peak;
+    }
+
+    return maxpeak;
+}
+
 static int filter_frame(AVFilterLink *inlink, AVFrame *insamples)
 {
     int ret;
@@ -669,41 +688,25 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *insamples)
         if (ret < 0)
             return ret;
 
-        double maxpeak = 0.0;
-        for (int ch = 0; ch < nb_channels; ch++) {
-            double tp   = ebur128->true_peaks[ch];
-            double tppf = 0.0;
-            for (int i = 0; i < ret; i++) {
-                const double sample = fabs(swr_samples[i * nb_channels]);
-                tp   = FFMAX(tp,   sample);
-                tppf = FFMAX(tppf, sample);
-            }
-            maxpeak = FFMAX(maxpeak, tp);
-            ebur128->true_peaks[ch] = tp;
-            ebur128->true_peaks_per_frame[ch] = tppf;
-        }
+        memset(ebur128->true_peaks_per_frame, 0,
+               nb_channels * sizeof(*ebur128->true_peaks_per_frame));
 
-        ebur128->true_peak = DBFS(maxpeak);
+        double peak = dsp->find_peak(ebur128->true_peaks_per_frame, nb_channels,
+                                     swr_samples, ret);
+
+        ebur128->true_peak = DBFS(peak);
+        for (int ch = 0; ch < nb_channels; ch++) {
+            ebur128->true_peaks[ch] = FFMAX(ebur128->true_peaks[ch],
+                                            ebur128->true_peaks_per_frame[ch]);
+        }
     }
 #endif
 
     if (ebur128->peak_mode & PEAK_MODE_SAMPLES_PEAKS) {
-        double maxpeak = 0.0;
-        for (int ch = 0; ch < nb_channels; ch++) {
-            const double *restrict samples_ch = &samples[ch];
-            double sp = ebur128->sample_peaks[ch];
-
-            for (int i = ebur128->idx_insample; i < nb_samples; i++) {
-                const double sample = fabs(samples_ch[nb_channels * i]);
-                sp = FFMAX(sp, sample);
-            }
-            maxpeak = FFMAX(maxpeak, sp);
-            ebur128->sample_peaks[ch] = sp;
-        }
-
-        ebur128->sample_peak = DBFS(maxpeak);
+        double peak = dsp->find_peak(ebur128->sample_peaks, nb_channels,
+                                     samples, nb_samples);
+        ebur128->sample_peak = DBFS(peak);
     }
-
 
     for (int idx_insample = ebur128->idx_insample; idx_insample < nb_samples; idx_insample++) {
         const int bin_id_400  = ebur128->i400.cache_pos++;

libavfilter/f_ebur128.h
@@ -45,6 +45,10 @@ typedef struct EBUR128DSPContext {
                             double *cache_400, double *cache_3000,
                             double *sum_400, double *sum_3000,
                             int nb_channels);
+
+    /* Updates ch_peaks[] and returns maximum from all channels */
+    double (*find_peak)(double *ch_peaks, int nb_channels,
+                        const double *samples, int nb_samples);
 } EBUR128DSPContext;
 
 static_assert(offsetof(EBUR128DSPContext, pre) == 0,                   "struct layout mismatch");
@@ -55,5 +59,7 @@ void ff_ebur128_init_x86(EBUR128DSPContext *dsp);
 
 void ff_ebur128_filter_channels_c(const EBUR128DSPContext *, const double *,
                                   double *, double *, double *, double *, int);
+
+double ff_ebur128_find_peak_c(double *, int, const double *, int);
 
 #endif /* AVFILTER_F_EBUR128_H */

commit 4603da38d8aeb52b31ef0dedf5c9fc78d96c6aea

avfilter/f_ebur128: properly propagate true peak
After 681eeed356, `ebur128->true_peak` was only set to the maximum of the
current "true peak per frame" values, when it should report the true peak for
the entire stream.

Fixes: 681eeed356cbe3a6c15b18e6ecac71bbba40fbf0

libavfilter/f_ebur128.c
@@ -692,11 +692,13 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *insamples)
         double peak = dsp->find_peak(ebur128->true_peaks_per_frame, nb_channels,
                                      swr_samples, ret);
 
-        ebur128->true_peak = DBFS(peak);
         for (int ch = 0; ch < nb_channels; ch++) {
+            peak = FFMAX(peak, ebur128->true_peaks[ch]);
             ebur128->true_peaks[ch] = FFMAX(ebur128->true_peaks[ch],
                                             ebur128->true_peaks_per_frame[ch]);
         }
+
+        ebur128->true_peak = DBFS(peak);
     }
 #endif
 