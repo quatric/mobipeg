jamrial/FFmpeg

avfilter/vf_showinfo: add support for 3D Reference Displays Information side data
Signed-off-by: James Almer <jamrial@gmail.com>

libavfilter/vf_showinfo.c
@@ -38,6 +38,7 @@
 #include "libavutil/pixdesc.h"
 #include "libavutil/spherical.h"
 #include "libavutil/stereo3d.h"
+#include "libavutil/ref_displays_info.h"
 #include "libavutil/timestamp.h"
 #include "libavutil/timecode.h"
 #include "libavutil/mastering_display_metadata.h"
@@ -150,6 +151,13 @@ static void dump_roi(AVFilterContext *ctx, const AVFrameSideData *sd)
         av_log(ctx, AV_LOG_INFO, "index: %d, region: (%d, %d) -> (%d, %d), qp offset: %d/%d.\n",
                i, roi->left, roi->top, roi->right, roi->bottom, roi->qoffset.num, roi->qoffset.den);
     }
+}
+
+static void dump_ref_displays_info(AVFilterContext *ctx, const AVFrameSideData *sd)
+{
+    const AV3DReferenceDisplaysInfo *rdi = (const AV3DReferenceDisplaysInfo *)sd->data;
+
+    av_log(ctx, AV_LOG_INFO, "number of reference displays: %u", rdi->num_ref_displays);
 }
 
 static void dump_detection_bbox(AVFilterContext *ctx, const AVFrameSideData *sd)
@@ -859,6 +867,9 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *frame)
             break;
         case AV_FRAME_DATA_VIEW_ID:
             av_log(ctx, AV_LOG_INFO, "view id: %d\n", *(int*)sd->data);
+            break;
+        case AV_FRAME_DATA_3D_REFERENCE_DISPLAYS:
+            dump_ref_displays_info(ctx, sd);
             break;
         default:
             if (name)

quatric/mobipeg

libavfilter/vf_showinfo.c
@@ -38,6 +38,7 @@
 #include "libavutil/pixdesc.h"
 #include "libavutil/spherical.h"
 #include "libavutil/stereo3d.h"
+#include "libavutil/tdrdi.h"
 #include "libavutil/timestamp.h"
 #include "libavutil/timecode.h"
 #include "libavutil/mastering_display_metadata.h"
@@ -150,6 +151,14 @@ static void dump_roi(AVFilterContext *ctx, const AVFrameSideData *sd)
         av_log(ctx, AV_LOG_INFO, "index: %d, region: (%d, %d) -> (%d, %d), qp offset: %d/%d.\n",
                i, roi->left, roi->top, roi->right, roi->bottom, roi->qoffset.num, roi->qoffset.den);
     }
+}
+
+static void dump_tdrdi(AVFilterContext *ctx, const AVFrameSideData *sd)
+{
+    const AV3DReferenceDisplaysInfo *tdrdi = (const AV3DReferenceDisplaysInfo *)sd->data;
+
+
+    av_log(ctx, AV_LOG_INFO, "number of reference displays: %u", tdrdi->num_ref_displays);
 }
 
 static void dump_detection_bbox(AVFilterContext *ctx, const AVFrameSideData *sd)
@@ -859,6 +868,9 @@ static int filter_frame(AVFilterLink *inlink, AVFrame *frame)
             break;
         case AV_FRAME_DATA_VIEW_ID:
             av_log(ctx, AV_LOG_INFO, "view id: %d\n", *(int*)sd->data);
+            break;
+        case AV_FRAME_DATA_3D_REFERENCE_DISPLAYS:
+            dump_tdrdi(ctx, sd);
             break;
         default:
             if (name)