quatric/mobipeg

commit 343eb257d9de52db38098a2e9d626e658098f8db

avformat/mov: export the correct initial extratada from samples with multiple stsd
The first sample in the stsc box may not refer to the first stsd entry.
This is the case in h264/thezerotheorem-cut.mp4, and as such the
fate-h264_redundant_pps-side_data test is updated accordingly.

Signed-off-by: James Almer <jamrial@gmail.com>

libavformat/mov.c
@@ -10580,6 +10580,20 @@ static int mov_read_header(AVFormatContext *s)
         MOVStreamContext *sc = st->priv_data;
         uint32_t dvdsub_clut[FF_DVDCLUT_CLUT_LEN] = {0};
         fix_timescale(mov, sc);
+
+        /* Set the primary extradata based on the first Sample. */
+        if (sc->stsc_count && sc->extradata_size && !sc->iamf) {
+            sc->last_stsd_index = sc->stsc_data[0].id - 1;
+            av_freep(&st->codecpar->extradata);
+            st->codecpar->extradata_size = sc->extradata_size[sc->last_stsd_index];
+            if (sc->extradata_size[sc->last_stsd_index]) {
+                st->codecpar->extradata = av_mallocz(sc->extradata_size[sc->last_stsd_index] + AV_INPUT_BUFFER_PADDING_SIZE);
+                if (!st->codecpar->extradata)
+                    return AVERROR(ENOMEM);
+                memcpy(st->codecpar->extradata, sc->extradata[sc->last_stsd_index], sc->extradata_size[sc->last_stsd_index]);
+            }
+        }
+
         if (st->codecpar->codec_type == AVMEDIA_TYPE_AUDIO &&
             st->codecpar->codec_id   == AV_CODEC_ID_AAC) {
             sti->skip_samples = sc->start_pad;

tests/ref/fate/h264_redundant_pps-side_data
@@ -1,12 +1,12 @@
-a35cca13c3f91d1a279bf576b8264d05 *tests/data/fate/h264_redundant_pps-side_data.nut
-596153 tests/data/fate/h264_redundant_pps-side_data.nut
-#extradata 0:       34, 0x851f08e4
+92fe70291f72acf94ba56b426bbaccb0 *tests/data/fate/h264_redundant_pps-side_data.nut
+596100 tests/data/fate/h264_redundant_pps-side_data.nut
+#extradata 0:       34, 0x850408e3
 #tb 0: 1/48000
 #media_type 0: video
 #codec_id 0: h264
 #dimensions 0: 1920x1080
 #sar 0: 0/1
-0,      -2002,          0,     2002,   247959, 0xdb721881, S=1, New Extradata,       34, 0x850408e3
+0,      -2002,          0,     2002,   247959, 0xdb721881
 0,          0,       4004,     2002,    43356, 0xa366eb79, F=0x0
 0,       2002,       2002,     2002,    11423, 0x9c0a86fa, F=0x0
 0,       4004,       8008,     2002,    50801, 0xfbfe860d, F=0x0

commit 46265a656045795bff32dd6c4564f1bbe062e319

avformat/mov: move AVC-Intra extradata generation to earlier in the stsd parsing process
This will ensure it will be stored in the stream's private context, so it can
be fetched and replaced as required.

Fixes a regression since 343eb257d9de52db38098a2e9d626e658098f8db.
Fixes avc-intra-panasonic-AG-HPX301E.mov

Signed-off-by: James Almer <jamrial@gmail.com>

libavformat/mov.c
@@ -3028,6 +3028,27 @@ static int mov_skip_multiple_stsd(MOVContext *c, AVIOContext *pb,
     return 0;
 }
 
+static int mov_finalize_stsd_entry(MOVContext *c, AVStream *st)
+{
+    int ret;
+
+    /* special codec parameters handling */
+    switch (st->codecpar->codec_id) {
+    case AV_CODEC_ID_H264:
+        // done for ai5q, ai52, ai55, ai1q, ai12 and ai15.
+        if (!st->codecpar->extradata_size && TAG_IS_AVCI(st->codecpar->codec_tag)) {
+            ret = ff_generate_avci_extradata(st);
+            if (ret < 0)
+               return ret;
+        }
+        break;
+    default:
+        break;
+    }
+
+    return 0;
+}
+
 int ff_mov_read_stsd_entries(MOVContext *c, AVIOContext *pb, int entries)
 {
     AVStream *st;
@@ -3104,6 +3125,10 @@ int ff_mov_read_stsd_entries(MOVContext *c, AVIOContext *pb, int entries)
                 return ret;
         } else if (a.size > 0)
             avio_skip(pb, a.size);
+
+        ret = mov_finalize_stsd_entry(c, st);
+        if (ret < 0)
+            return ret;
 
         if (sc->extradata && st->codecpar->extradata) {
             int extra_size = st->codecpar->extradata_size;
@@ -5214,14 +5239,6 @@ static int mov_read_trak(MOVContext *c, AVIOContext *pb, MOVAtom atom)
             av_reduce(&st->r_frame_rate.num, &st->r_frame_rate.den,
                       sc->time_scale, sc->tts_data[0].duration, INT_MAX);
 #endif
-    }
-
-    // done for ai5q, ai52, ai55, ai1q, ai12 and ai15.
-    if (!st->codecpar->extradata_size && st->codecpar->codec_id == AV_CODEC_ID_H264 &&
-        TAG_IS_AVCI(st->codecpar->codec_tag)) {
-        ret = ff_generate_avci_extradata(st);
-        if (ret < 0)
-            return ret;
     }
 
     switch (st->codecpar->codec_id) {