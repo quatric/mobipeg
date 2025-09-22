librempeg/librempeg

avcodec: add ADPCM IMA HVQM2 decoder

libavcodec/adpcm.c
@@ -537,6 +537,43 @@ static inline int adpcm_ima_qt_expand_nibble(ADPCMChannelStatus *c, int nibble)
     return c->predictor;
 }
 
+static void decode_adpcm_ima_hvqm2(AVCodecContext *avctx, int16_t *outbuf, int samples_to_do,
+                                   int frame_format, GetByteContext *gb)
+{
+    ADPCMDecodeContext *c = avctx->priv_data;
+    int st = avctx->ch_layout.nb_channels == 2;
+    uint8_t nibble;
+
+    for (int ch = 0; ch < avctx->ch_layout.nb_channels; ch++) {
+        unsigned tmp;
+
+        switch (frame_format) {
+        case 0: /* combined hist+index */
+            tmp = bytestream2_get_be16(gb);
+            c->status[ch].predictor  = sign_extend(tmp & 0xFF80, 16);
+            c->status[ch].step_index = tmp & 0x7f;
+            *outbuf++ = c->status[ch].predictor;
+            samples_to_do--;
+            break;
+        default:
+            break;
+        }
+
+        c->status[ch].step_index = av_clip(c->status[ch].step_index, 0, 88);
+    }
+
+    for (int i = 0; i < samples_to_do; i++) {
+        if (!(i&1)) {
+            nibble = bytestream2_get_byte(gb);
+            *outbuf++ = adpcm_ima_qt_expand_nibble(&c->status[st], nibble >>  4);
+        } else {
+            *outbuf++ = adpcm_ima_qt_expand_nibble(&c->status[ 0], nibble & 0xF);
+        }
+    }
+
+    bytestream2_seek(gb, 0, SEEK_END);
+}
+
 static void decode_adpcm_ima_hvqm4(AVCodecContext *avctx, int16_t *outbuf, int samples_to_do,
                                    int frame_format, GetByteContext *gb)
 {
@@ -972,6 +1009,9 @@ static int get_nb_samples(AVCodecContext *avctx, GetByteContext *gb,
         *coded_samples  = bytestream2_get_le32(gb);
         *coded_samples -= *coded_samples % 28;
         nb_samples      = (buf_size - 12) / (ch == 2 ? 30 : 15) * 28;
+        break;
+    case AV_CODEC_ID_ADPCM_IMA_HVQM2:
+        nb_samples = ((bytestream2_peek_be64(gb) >> 16) & 0xFFFF);
         break;
     case AV_CODEC_ID_ADPCM_IMA_HVQM4:
         {
@@ -1571,6 +1611,12 @@ static int adpcm_decode_frame(AVCodecContext *avctx, AVFrame *frame,
             *samples++ = adpcm_ima_expand_nibble(&c->status[0],  v >> 4  , 3);
             *samples++ = adpcm_ima_expand_nibble(&c->status[st], v & 0x0F, 3);
         }
+        ) /* End of CASE */
+    CASE(ADPCM_IMA_HVQM2,
+        int format = bytestream2_get_be16(&gb);
+
+        bytestream2_skip(&gb, 4);
+        decode_adpcm_ima_hvqm2(avctx, samples, nb_samples, format, &gb);
         ) /* End of CASE */
     CASE(ADPCM_IMA_HVQM4,
         int format = bytestream2_get_be16(&gb);
@@ -2607,6 +2653,7 @@ ADPCM_DECODER(ADPCM_IMA_DK3,     sample_fmts_s16,  adpcm_ima_dk3,     "ADPCM IMA
 ADPCM_DECODER(ADPCM_IMA_DK4,     sample_fmts_s16,  adpcm_ima_dk4,     "ADPCM IMA Duck DK4")
 ADPCM_DECODER(ADPCM_IMA_EA_EACS, sample_fmts_s16,  adpcm_ima_ea_eacs, "ADPCM IMA Electronic Arts EACS")
 ADPCM_DECODER(ADPCM_IMA_EA_SEAD, sample_fmts_s16,  adpcm_ima_ea_sead, "ADPCM IMA Electronic Arts SEAD")
+ADPCM_DECODER(ADPCM_IMA_HVQM2,   sample_fmts_s16,  adpcm_ima_hvqm2,   "ADPCM IMA HVQM2")
 ADPCM_DECODER(ADPCM_IMA_HVQM4,   sample_fmts_s16,  adpcm_ima_hvqm4,   "ADPCM IMA HVQM4")
 ADPCM_DECODER(ADPCM_IMA_ISS,     sample_fmts_s16,  adpcm_ima_iss,     "ADPCM IMA Funcom ISS")
 ADPCM_DECODER(ADPCM_IMA_MOFLEX,  sample_fmts_s16p, adpcm_ima_moflex,  "ADPCM IMA MobiClip MOFLEX")

quatric/mobipeg

(cherry picked from commit 480e36592d5fc27a47e378d62570824613f26b7b)

libavcodec/adpcm.c
@@ -548,6 +548,43 @@ int16_t ff_adpcm_ima_qt_expand_nibble(ADPCMChannelStatus *c, int nibble)
     return c->predictor;
 }
 
+static void decode_adpcm_ima_hvqm2(AVCodecContext *avctx, int16_t *outbuf, int samples_to_do,
+                                   int frame_format, GetByteContext *gb)
+{
+    ADPCMDecodeContext *c = avctx->priv_data;
+    int st = avctx->ch_layout.nb_channels == 2;
+    uint8_t nibble;
+
+    for (int ch = 0; ch < avctx->ch_layout.nb_channels; ch++) {
+        unsigned tmp;
+
+        switch (frame_format) {
+        case 0: /* combined hist+index */
+            tmp = bytestream2_get_be16(gb);
+            c->status[ch].predictor  = sign_extend(tmp & 0xFF80, 16);
+            c->status[ch].step_index = tmp & 0x7f;
+            *outbuf++ = c->status[ch].predictor;
+            samples_to_do--;
+            break;
+        default:
+            break;
+        }
+
+        c->status[ch].step_index = av_clip(c->status[ch].step_index, 0, 88);
+    }
+
+    for (int i = 0; i < samples_to_do; i++) {
+        if (!(i&1)) {
+            nibble = bytestream2_get_byte(gb);
+            *outbuf++ = ff_adpcm_ima_qt_expand_nibble(&c->status[st], nibble >>  4);
+        } else {
+            *outbuf++ = ff_adpcm_ima_qt_expand_nibble(&c->status[ 0], nibble & 0xF);
+        }
+    }
+
+    bytestream2_seek(gb, 0, SEEK_END);
+}
+
 static void decode_adpcm_ima_hvqm4(AVCodecContext *avctx, int16_t *outbuf, int samples_to_do,
                                    int frame_format, GetByteContext *gb)
 {
@@ -1149,6 +1186,9 @@ static int get_nb_samples(AVCodecContext *avctx, GetByteContext *gb,
         *coded_samples  = bytestream2_get_le32(gb);
         *coded_samples -= *coded_samples % 28;
         nb_samples      = (buf_size - 12) / (ch == 2 ? 30 : 15) * 28;
+        break;
+    case AV_CODEC_ID_ADPCM_IMA_HVQM2:
+        nb_samples = ((bytestream2_peek_be64(gb) >> 16) & 0xFFFF);
         break;
     case AV_CODEC_ID_ADPCM_IMA_HVQM4:
         {
@@ -1754,6 +1794,12 @@ static int adpcm_decode_frame(AVCodecContext *avctx, AVFrame *frame,
             *samples++ = adpcm_ima_expand_nibble(&c->status[0],  v >> 4  , 3);
             *samples++ = adpcm_ima_expand_nibble(&c->status[st], v & 0x0F, 3);
         }
+        ) /* End of CASE */
+    CASE(ADPCM_IMA_HVQM2,
+        int format = bytestream2_get_be16(&gb);
+
+        bytestream2_skip(&gb, 4);
+        decode_adpcm_ima_hvqm2(avctx, samples, nb_samples, format, &gb);
         ) /* End of CASE */
     CASE(ADPCM_IMA_HVQM4,
         int format = bytestream2_get_be16(&gb);
@@ -2813,6 +2859,7 @@ ADPCM_DECODER(ADPCM_IMA_DK3,     sample_fmts_s16,  adpcm_ima_dk3,     "ADPCM IMA
 ADPCM_DECODER(ADPCM_IMA_DK4,     sample_fmts_s16,  adpcm_ima_dk4,     "ADPCM IMA Duck DK4")
 ADPCM_DECODER(ADPCM_IMA_EA_EACS, sample_fmts_s16,  adpcm_ima_ea_eacs, "ADPCM IMA Electronic Arts EACS")
 ADPCM_DECODER(ADPCM_IMA_EA_SEAD, sample_fmts_s16,  adpcm_ima_ea_sead, "ADPCM IMA Electronic Arts SEAD")
+ADPCM_DECODER(ADPCM_IMA_HVQM2,   sample_fmts_s16,  adpcm_ima_hvqm2,   "ADPCM IMA HVQM2")
 ADPCM_DECODER(ADPCM_IMA_HVQM4,   sample_fmts_s16,  adpcm_ima_hvqm4,   "ADPCM IMA HVQM4")
 ADPCM_DECODER(ADPCM_IMA_ISS,     sample_fmts_s16,  adpcm_ima_iss,     "ADPCM IMA Funcom ISS")
 ADPCM_DECODER(ADPCM_IMA_MOFLEX,  sample_fmts_s16p, adpcm_ima_moflex,  "ADPCM IMA MobiClip MOFLEX")