quatric/mobipeg

commit e5805ccbb4f1928a41b88492ba61b35530f94a13

avformat/mov: add overflow checks to item offset values
Fixes issue #21478.

Signed-off-by: James Almer <jamrial@gmail.com>

libavformat/mov.c
@@ -10252,6 +10252,9 @@ static int read_image_grid(AVFormatContext *s, const HEIFGrid *grid,
         offset = c->idat_offset;
     }
 
+    if (offset > INT64_MAX - item->extent_offset)
+        return AVERROR_INVALIDDATA;
+
     avio_seek(s->pb, item->extent_offset + offset, SEEK_SET);
 
     avio_r8(s->pb);    /* version */
@@ -10335,6 +10338,9 @@ static int read_image_iovl(AVFormatContext *s, const HEIFGrid *grid,
         offset = c->idat_offset;
     }
 
+    if (offset > INT64_MAX - item->extent_offset)
+        return AVERROR_INVALIDDATA;
+
     avio_seek(s->pb, item->extent_offset + offset, SEEK_SET);
 
     avio_r8(s->pb);    /* version */
@@ -10407,6 +10413,9 @@ static int mov_parse_exif_item(AVFormatContext *s,
     buf = av_buffer_alloc(ref->extent_length);
     if (!buf)
         return AVERROR(ENOMEM);
+
+    if (offset > INT64_MAX - ref->extent_offset)
+        return AVERROR_INVALIDDATA;
 
     avio_seek(s->pb, ref->extent_offset + offset, SEEK_SET);
     err = avio_read(s->pb, buf->data, ref->extent_length);
@@ -10619,6 +10628,9 @@ static int mov_parse_heif_items(AVFormatContext *s)
 
         err = sanity_checks(s, sc, st->index);
         if (err)
+            return AVERROR_INVALIDDATA;
+
+        if (offset > INT64_MAX - item->extent_offset)
             return AVERROR_INVALIDDATA;
 
         sc->chunk_offsets[0] = item->extent_offset + offset;

commit 9b3235c1c515915a3e7897f0fa13ef7904066e66

avformat/mov: fix memory leak on error
Regression since e5805ccbb4f1928a41b88492ba61b35530f94a13.

Fixes CID 1680454.

Signed-off-by: James Almer <jamrial@gmail.com>

libavformat/mov.c
@@ -10414,8 +10414,10 @@ static int mov_parse_exif_item(AVFormatContext *s,
     if (!buf)
         return AVERROR(ENOMEM);
 
-    if (offset > INT64_MAX - ref->extent_offset)
-        return AVERROR_INVALIDDATA;
+    if (offset > INT64_MAX - ref->extent_offset) {
+        err = AVERROR(ENOMEM);
+        goto fail;
+    }
 
     avio_seek(s->pb, ref->extent_offset + offset, SEEK_SET);
     err = avio_read(s->pb, buf->data, ref->extent_length);