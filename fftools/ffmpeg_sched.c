quatric/mobipeg

commit 22baf2813f490914e50e4efa90c9c55ca3296b39

fftools/ffmpeg_sched: unchoke upstream nodes on recv-closed filter inputs
This allows upstream filters to observe EOF on their corresponding outputs
and terminate early, which is particularly useful for decoders and demuxers
that may then gracefully release their resources.

Prevents "leaking" memory for previously used, but now unused, filter inputs.

fftools/ffmpeg_sched.c
@@ -1368,6 +1368,18 @@ static void schedule_update_locked(Scheduler *sch)
         }
     }
 
+    // also unchoke any sources feeding into closed filter graph inputs, so
+    // that they can observe the downstream EOF
+    for (unsigned i = 0; i < sch->nb_filters; i++) {
+        SchFilterGraph *fg = &sch->filters[i];
+
+        for (unsigned j = 0; j < fg->nb_inputs; j++) {
+            SchFilterIn *fi = &fg->inputs[j];
+            if (fi->receive_finished && !fi->send_finished)
+                unchoke_for_stream(sch, fi->src);
+        }
+    }
+
     // make sure to unchoke at least one source, if still available
     for (unsigned type = 0; !have_unchoked && type < 2; type++)
         for (unsigned i = 0; i < (type ? sch->nb_filters : sch->nb_demux); i++) {
@@ -2478,6 +2490,8 @@ void sch_filter_receive_finish(Scheduler *sch, unsigned fg_idx, unsigned in_idx)
     av_assert0(in_idx < fg->nb_inputs);
     fi = &fg->inputs[in_idx];
 
+    pthread_mutex_lock(&sch->schedule_lock);
+
     if (!fi->receive_finished) {
         fi->receive_finished = 1;
         tq_receive_finish(fg->queue, in_idx);
@@ -2485,7 +2499,11 @@ void sch_filter_receive_finish(Scheduler *sch, unsigned fg_idx, unsigned in_idx)
         // close the control stream when all actual inputs are done
         if (++fg->nb_inputs_finished_receive == fg->nb_inputs)
             tq_receive_finish(fg->queue, fg->nb_inputs);
+
+        schedule_update_locked(sch);
     }
+
+    pthread_mutex_unlock(&sch->schedule_lock);
 }
 
 int sch_filter_send(Scheduler *sch, unsigned fg_idx, unsigned out_idx, AVFrame *frame)

commit 772d308f66b0f22ae81a4929890c3dc0b4a36fa5

fftools/ffmpeg_sched: fix race on SchFilterIn.send_finished
This logic was previously added to the scheduler. That commit added locking
(and rescheduling) when updating the corresponding `receive_finished` flag,
but missed doing the same for `send_finished`.

Fixes: 22baf2813f490914e50e4efa90c9c55ca3296b39

fftools/ffmpeg_sched.c
@@ -2297,6 +2297,8 @@ static int send_to_filter(Scheduler *sch, SchFilterGraph *fg,
     if (frame)
         return tq_send(fg->queue, in_idx, frame);
 
+    pthread_mutex_lock(&sch->schedule_lock);
+
     if (!fg->inputs[in_idx].send_finished) {
         fg->inputs[in_idx].send_finished = 1;
         tq_send_finish(fg->queue, in_idx);
@@ -2304,7 +2306,11 @@ static int send_to_filter(Scheduler *sch, SchFilterGraph *fg,
         // close the control stream when all actual inputs are done
         if (atomic_fetch_add(&fg->nb_inputs_finished_send, 1) == fg->nb_inputs - 1)
             tq_send_finish(fg->queue, fg->nb_inputs);
+
+        schedule_update_locked(sch);
     }
+
+    pthread_mutex_unlock(&sch->schedule_lock);
     return 0;
 }
 