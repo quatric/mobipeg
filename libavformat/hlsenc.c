quatric/mobipeg

commit 364c64698adc840fb3842e0e6cb6a39dda520c40

avformat/hlsenc: fix format string vulnerability in parse_playlist
Found-by: Sarthak Munshi <sarthakmunshi@gmail.com>
Signed-off-by: Michael Niedermayer <michael@niedermayer.cc>

libavformat/hlsenc.c
@@ -1242,13 +1242,13 @@ static int parse_playlist(AVFormatContext *s, const char *url, VariantStream *vs
                 if (vs->has_subtitle) {
                     int vtt_index = extract_segment_number(line);
                     const char *vtt_basename = av_basename(vs->vtt_basename);
-                    int len = strlen(vtt_basename) + 11;
-                    char *vtt_file = av_mallocz(len);
-                    if (!vtt_file) {
+                    char *vtt_file = NULL;
+                    int ret = replace_int_data_in_filename(&vtt_file, vtt_basename, 'd', vtt_index);
+                    if (ret < 0 || !vtt_file) {
                         ret = AVERROR(ENOMEM);
                         goto fail;
                     }
-                    snprintf(vtt_file, len, vtt_basename, vtt_index);
+
                     ff_format_set_url(vs->vtt_avf, vtt_file);
                 }
 

commit fd77eaeef4612f61abf14b93fa0cf248f30faca0

avformat/hlsenc: Return error upon error, fix shadowing
Introduced in 364c64698adc840fb3842e0e6cb6a39dda520c40.

Reviewed-by: Marvin Scholz <epirat07@gmail.com>
Reviewed-by: Michael Niedermayer <michael@niedermayer.cc>
Signed-off-by: Andreas Rheinhardt <andreas.rheinhardt@outlook.com>

libavformat/hlsenc.c
@@ -1243,7 +1243,7 @@ static int parse_playlist(AVFormatContext *s, const char *url, VariantStream *vs
                     int vtt_index = extract_segment_number(line);
                     const char *vtt_basename = av_basename(vs->vtt_basename);
                     char *vtt_file = NULL;
-                    int ret = replace_int_data_in_filename(&vtt_file, vtt_basename, 'd', vtt_index);
+                    ret = replace_int_data_in_filename(&vtt_file, vtt_basename, 'd', vtt_index);
                     if (ret < 0 || !vtt_file) {
                         ret = AVERROR(ENOMEM);
                         goto fail;