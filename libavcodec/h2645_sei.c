quatric/mobipeg

commit 4c2d7375f1cd5c84a8090f6074d72f2207f4c25e

avcodec/h2645_sei: fix parsing payloads for UK country_code
The correct syntax after country_code is:

t35_uk_country_code_second_octet      b(8)
t35_uk_manufacturer_code_first_octet  b(8)
t35_uk_manufacturer_code_second_octet b(8)

Signed-off-by: James Almer <jamrial@gmail.com>

libavcodec/h2645_sei.c
@@ -159,10 +159,11 @@ static int decode_registered_user_data(H2645SEI *h, GetByteContext *gb,
     }
 
     /* itu_t_t35_payload_byte follows */
-    provider_code = bytestream2_get_be16u(gb);
 
     switch (country_code) {
     case ITU_T_T35_COUNTRY_CODE_US:
+        provider_code = bytestream2_get_be16u(gb);
+
         switch (provider_code) {
         case ITU_T_T35_PROVIDER_CODE_ATSC: {
             uint32_t user_identifier;
@@ -230,12 +231,17 @@ static int decode_registered_user_data(H2645SEI *h, GetByteContext *gb,
         }
         break;
     case ITU_T_T35_COUNTRY_CODE_UK:
+        bytestream2_skipu(gb, 1); // t35_uk_country_code_second_octet
+        if (bytestream2_get_bytes_left(gb) < 2)
+            return AVERROR_INVALIDDATA;
+
+        provider_code = bytestream2_get_be16u(gb);
+
         switch (provider_code) {
         case ITU_T_T35_PROVIDER_CODE_VNOVA:
             if (bytestream2_get_bytes_left(gb) < 2)
                 return AVERROR_INVALIDDATA;
 
-            bytestream2_skipu(gb, 1); // user_data_type_code
             return decode_registered_user_data_lcevc(&h->lcevc, gb);
         default:
             break;
@@ -245,6 +251,8 @@ static int decode_registered_user_data(H2645SEI *h, GetByteContext *gb,
     case ITU_T_T35_COUNTRY_CODE_CN: {
         const uint16_t cuva_provider_oriented_code = 0x0005;
         uint16_t provider_oriented_code;
+
+        provider_code = bytestream2_get_be16u(gb);
 
         switch (provider_code) {
         case ITU_T_T35_PROVIDER_CODE_HDR_VIVID:

libavcodec/itut35.h
@@ -30,10 +30,8 @@
 // - CN providers
 #define ITU_T_T35_PROVIDER_CODE_HDR_VIVID    0x0004
 // - UK providers
-// V-Nova should be 0x5000 according to UK Register of Manufacturer Codes
 // https://www.cix.co.uk/~bpechey/H221/h221code.htm
-// but FFmpeg has been using 0x0050
-#define ITU_T_T35_PROVIDER_CODE_VNOVA        0x0050
+#define ITU_T_T35_PROVIDER_CODE_VNOVA        0x5000
 // - US providers
 #define ITU_T_T35_PROVIDER_CODE_ATSC         0x0031
 #define ITU_T_T35_PROVIDER_CODE_DOLBY        0x003B

commit 8f8c7c0704f6d0483e694b3f7415696e8337e768

avcodec/h2645_sei: don't use provider_code uninitialized
Regression since 4c2d7375f1cd5c84a8090f6074d72f2207f4c25e.
Fixes Coverity issue CID 1689618.

Signed-off-by: James Almer <jamrial@gmail.com>

libavcodec/h2645_sei.c
@@ -145,7 +145,7 @@ static int decode_registered_user_data_closed_caption(H2645SEIA53Caption *h,
 static int decode_registered_user_data(H2645SEI *h, GetByteContext *gb,
                                        enum AVCodecID codec_id, void *logctx)
 {
-    int country_code, provider_code;
+    int country_code, provider_code = -1;
 
     if (bytestream2_get_bytes_left(gb) < 3)
         return AVERROR_INVALIDDATA;