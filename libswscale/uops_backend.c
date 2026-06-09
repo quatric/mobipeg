ramiropolla/ffmpeg

swscale/uops: auto-generate reference C backend from uops_macros.h
Instead of choosing by hand which kernels to implement, this rewrite focuses
on leveraging the power of uops_macros.h to auto-generate all needed kernels.
This not only simplifies maintenance, but also improves performance.

I have decided to develop the replacement backend as a separate file, under
a separate prefix, for the explicit purpose of being able to verify the
correctness of the rewrite using the current backend as a checkasm reference.

The code for the kernels themselves has been largely copied from the old
C backend, modified slightly to conform to the uop template style. This does
result in some code duplication, but a following commit will clean it up.
I nonetheless want to preserve this commit for bisection purposes, to ensure
we have one commit that contains both backends side-by-side.

Overall speedup=1.182x faster, min=0.197x max=3.450x

The big slowdowns are flukes caused by tiny deviations in the runtime of
a noop memcpy conversion.

As a nice side benefit, the compiled binary is now also ~10% smaller, and
the code ~50% smaller.

Signed-off-by: Niklas Haas <git@haasn.dev>

libswscale/uops_backend.c
@@ -1,0 +1,193 @@
+/**
+ * Copyright (C) 2026 Niklas Haas
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include "libavutil/avassert.h"
+
+#include "uops_tmpl.h"
+
+/**
+ * We want to disable FP contraction because this is a reference backend that
+ * establishes a bit-exact reference result.
+ */
+#ifdef __clang__
+#pragma STDC FP_CONTRACT OFF
+#elif AV_GCC_VERSION_AT_LEAST(4, 8)
+#pragma GCC optimize ("fp-contract=off")
+#elif defined(_MSC_VER)
+#pragma fp_contract (off)
+#endif
+
+#if AV_GCC_VERSION_AT_LEAST(4, 4)
+#pragma GCC optimize ("finite-math-only")
+#endif
+
+/* Integer types */
+#define IS_FLOAT 0
+#  define BIT_DEPTH 8
+#    include "uops_tmpl.c"
+#  undef BIT_DEPTH
+#  define BIT_DEPTH 16
+#    include "uops_tmpl.c"
+#  undef BIT_DEPTH
+#  define BIT_DEPTH 32
+#    include "uops_tmpl.c"
+#  undef BIT_DEPTH
+#undef IS_FLOAT
+
+/* Floating point types */
+#define IS_FLOAT 1
+#  define BIT_DEPTH 32
+#    include "uops_tmpl.c"
+#  undef BIT_DEPTH
+#undef IS_FLOAT
+
+/* Expanded as new uop types are implemented in the C/template backend */
+#define REF_ALL_UOPS(TYPE)                                  \
+    SWS_FOR(READ_PLANAR,    TYPE, REF_ENTRY)                \
+    SWS_FOR(READ_PLANAR_FV, TYPE, REF_ENTRY)                \
+    SWS_FOR(READ_PLANAR_FH, TYPE, REF_ENTRY)                \
+    SWS_FOR(READ_PACKED,    TYPE, REF_ENTRY)                \
+    SWS_FOR(READ_NIBBLE,    TYPE, REF_ENTRY)                \
+    SWS_FOR(READ_BIT,       TYPE, REF_ENTRY)                \
+    SWS_FOR(WRITE_PLANAR,   TYPE, REF_ENTRY)                \
+    SWS_FOR(WRITE_PACKED,   TYPE, REF_ENTRY)                \
+    SWS_FOR(WRITE_NIBBLE,   TYPE, REF_ENTRY)                \
+    SWS_FOR(WRITE_BIT,      TYPE, REF_ENTRY)                \
+    SWS_FOR(SWAP_BYTES,     TYPE, REF_ENTRY)                \
+    SWS_FOR(EXPAND_BIT,     TYPE, REF_ENTRY)                \
+    SWS_FOR(EXPAND_PAIR,    TYPE, REF_ENTRY)                \
+    SWS_FOR(EXPAND_QUAD,    TYPE, REF_ENTRY)                \
+    SWS_FOR(PERMUTE,        TYPE, REF_ENTRY)                \
+    SWS_FOR(COPY,           TYPE, REF_ENTRY)                \
+    SWS_FOR(CONVERT,        TYPE, REF_ENTRY)                \
+    SWS_FOR(SCALE,          TYPE, REF_ENTRY)                \
+    SWS_FOR(ADD,            TYPE, REF_ENTRY)                \
+    SWS_FOR(MIN,            TYPE, REF_ENTRY)                \
+    SWS_FOR(MAX,            TYPE, REF_ENTRY)                \
+    SWS_FOR(UNPACK,         TYPE, REF_ENTRY)                \
+    SWS_FOR(PACK,           TYPE, REF_ENTRY)                \
+    SWS_FOR(LSHIFT,         TYPE, REF_ENTRY)                \
+    SWS_FOR(RSHIFT,         TYPE, REF_ENTRY)                \
+    SWS_FOR(CLEAR,          TYPE, REF_ENTRY)                \
+    SWS_FOR(LINEAR,         TYPE, REF_ENTRY)                \
+    SWS_FOR(DITHER,         TYPE, REF_ENTRY)                \
+    /* end of macro */
+
+static const SwsOpTable op_table = {
+    .block_size = SWS_BLOCK_SIZE,
+    .uops = true,
+    .entries = {
+        REF_ALL_UOPS(U8)
+        REF_ALL_UOPS(U16)
+        REF_ALL_UOPS(U32)
+        REF_ALL_UOPS(F32)
+        NULL
+    },
+};
+
+static void process(const SwsOpExec *exec, const void *priv,
+                    const int bx_start, const int y_start,
+                    int bx_end, int y_end)
+{
+    const SwsOpChain *chain = priv;
+    const SwsOpImpl *impl = chain->impl;
+    block_t x, y, z, w; /* allocate enough space for any intermediate */
+
+    SwsOpIter iterdata;
+    SwsOpIter *iter = &iterdata; /* for CONTINUE() macro to work */
+    iter->exec = exec;
+    for (int i = 0; i < 4; i++) {
+        iter->in[i]  = (uintptr_t) exec->in[i];
+        iter->out[i] = (uintptr_t) exec->out[i];
+    }
+
+    for (iter->y = y_start; iter->y < y_end; iter->y++) {
+        for (int block = bx_start; block < bx_end; block++) {
+            iter->x = block * SWS_BLOCK_SIZE;
+            CONTINUE(&x, &y, &z, &w);
+        }
+
+        const int y_bump = exec->in_bump_y ? exec->in_bump_y[iter->y] : 0;
+        for (int i = 0; i < 4; i++) {
+            iter->in[i]  += exec->in_bump[i] + y_bump * exec->in_stride[i];
+            iter->out[i] += exec->out_bump[i];
+        }
+    }
+}
+
+static int compile(SwsContext *ctx, SwsOpList *ops, SwsCompiledOp *out)
+{
+    int ret;
+
+    SwsOpChain *chain = ff_sws_op_chain_alloc();
+    if (!chain)
+        return AVERROR(ENOMEM);
+
+    SwsUOpList *uops = ff_sws_uop_list_alloc();
+    if (!uops) {
+        ret = AVERROR(ENOMEM);
+        goto fail;
+    }
+
+    ret = ff_sws_ops_translate(ops, uops);
+    if (ret < 0)
+        goto fail;
+
+    av_assert0(uops->num_ops > 0);
+    for (int i = 0; i < uops->num_ops; i++) {
+        const SwsOpTable *table = &op_table;
+        ret = ff_sws_uop_lookup(ctx, &table, 1, &uops->ops[i],
+                                SWS_BLOCK_SIZE, chain);
+        if (ret < 0)
+            goto fail;
+    }
+
+    *out = (SwsCompiledOp) {
+        .slice_align = 1,
+        .block_size  = SWS_BLOCK_SIZE,
+        .cpu_flags   = chain->cpu_flags,
+        .over_read   = chain->over_read,
+        .over_write  = chain->over_write,
+        .priv        = chain,
+        .free        = ff_sws_op_chain_free_cb,
+        .func        = process,
+    };
+
+    av_log(ctx, AV_LOG_DEBUG, "Compiled micro-ops:\n");
+    for (int i = 0; i < uops->num_ops; i++) {
+        char name[SWS_UOP_NAME_MAX];
+        ff_sws_uop_name(&uops->ops[i], name);
+        av_log(ctx, AV_LOG_DEBUG, "    %s\n", name);
+    }
+
+    ff_sws_uop_list_free(&uops);
+    return 0;
+
+fail:
+    ff_sws_uop_list_free(&uops);
+    ff_sws_op_chain_free(chain);
+    return ret;
+}
+
+const SwsOpBackend backend_uops = {
+    .name       = "uops",
+    .compile    = compile,
+    .hw_format  = AV_PIX_FMT_NONE,
+};

libswscale/uops_tmpl.h
@@ -1,0 +1,146 @@
+/**
+ * Copyright (C) 2026 Niklas Haas
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef SWSCALE_UOPS_TMPL_H
+#define SWSCALE_UOPS_TMPL_H
+
+/**
+ * Helper macros for the C-based backend.
+ *
+ * To use these macros, `pixel_t` should be defined as the type of pixels.
+ */
+
+#include <assert.h>
+#include <float.h>
+#include <stdint.h>
+
+#include "libavutil/attributes.h"
+
+#include "ops_chain.h"
+#include "uops_macros.h"
+
+#ifndef SWS_BLOCK_SIZE
+#  define SWS_BLOCK_SIZE 32
+#endif
+
+typedef union block_t {
+    uint8_t   u8[SWS_BLOCK_SIZE];
+    uint16_t u16[SWS_BLOCK_SIZE];
+    uint32_t u32[SWS_BLOCK_SIZE];
+    float    f32[SWS_BLOCK_SIZE];
+} block_t;
+
+#define SIZEOF_BLOCK (sizeof(pixel_t) * SWS_BLOCK_SIZE)
+
+/**
+ * Internal context holding per-iter execution data. The data pointers will be
+ * directly incremented by the corresponding read/write functions.
+ */
+typedef struct SwsOpIter {
+    uintptr_t in[4];
+    uintptr_t out[4];
+    int x, y;
+
+    /* Link back to per-slice execution context */
+    const SwsOpExec *exec;
+} SwsOpIter;
+
+#ifdef __clang__
+#  define SWS_LOOP AV_PRAGMA(clang loop vectorize(assume_safety))
+#elif defined(__GNUC__)
+#  define SWS_LOOP AV_PRAGMA(GCC ivdep)
+#else
+#  define SWS_LOOP
+#endif
+
+/* Miscellaneous helpers */
+#define bitfn2(name, ext) name ## _ ## ext
+#define bitfn(name, ext)  bitfn2(name, ext)
+#define fn(name)          bitfn(name, PX)
+
+#define bump_ptr(ptr, bump) ((pixel_t *) ((uintptr_t) (ptr) + (bump)))
+
+/* Helpers for dealing with component masks */
+#define X SWS_COMP_TEST(mask, 0)
+#define Y SWS_COMP_TEST(mask, 1)
+#define Z SWS_COMP_TEST(mask, 2)
+#define W SWS_COMP_TEST(mask, 3)
+
+/* Helper macros to make writing common function signatures less painful */
+#define DECL_FUNC(NAME, ...)                                                    \
+    static av_always_inline void                                                \
+        fn(NAME)(SwsOpIter *restrict iter, const SwsOpImpl *restrict impl,      \
+                 pixel_t *restrict x, pixel_t *restrict y,                      \
+                 pixel_t *restrict z, pixel_t *restrict w,                      \
+                 __VA_ARGS__)
+
+#define DECL_READ(NAME, ...)                                                    \
+    DECL_FUNC(NAME, __VA_ARGS__,                                                \
+              const pixel_t *restrict in0, const pixel_t *restrict in1,         \
+              const pixel_t *restrict in2, const pixel_t *restrict in3)         \
+
+#define DECL_WRITE(NAME, ...)                                                   \
+    DECL_FUNC(NAME, __VA_ARGS__,                                                \
+              pixel_t *restrict out0, pixel_t *restrict out1,                   \
+              pixel_t *restrict out2, pixel_t *restrict out3)                   \
+
+#define CALL(NAME, ...) fn(NAME)(iter, impl, x, y, z, w, __VA_ARGS__)
+
+/* Helper macro to call into the next continuation with a given type */
+#define CONTINUE(...)                                                           \
+    ((void (*)(SwsOpIter *, const SwsOpImpl *,                                  \
+               void *restrict x, void *restrict y,                              \
+               void *restrict z, void *restrict w)) impl->cont)                 \
+        (iter, &impl[1], __VA_ARGS__)
+
+/* Helper macros for common op setup code */
+#define DECL_SETUP(NAME, PARAMS, OUT)                                           \
+    static av_unused int fn(NAME)(const SwsImplParams *PARAMS,                  \
+                                  SwsImplResult *OUT)
+
+/* Helper macro for declaring kernel entry points */
+#define DECL_IMPL(FUNC, NAME, UOP, TYPE, ...)                                   \
+    static av_flatten void NAME##_c(SwsOpIter *restrict iter,                   \
+                                    const SwsOpImpl *restrict impl,             \
+                                    void *restrict x, void *restrict y,         \
+                                    void *restrict z, void *restrict w)         \
+    {                                                                           \
+        CALL(FUNC, __VA_ARGS__);                                                \
+    }
+
+#define DECL_IMPL_READ(...)                                                     \
+    DECL_IMPL(__VA_ARGS__,                                                      \
+              (const pixel_t *) iter->in[0], (const pixel_t *) iter->in[1],     \
+              (const pixel_t *) iter->in[2], (const pixel_t *) iter->in[3])
+
+#define DECL_IMPL_WRITE(...)                                                    \
+    DECL_IMPL(__VA_ARGS__,                                                      \
+              (pixel_t *) iter->out[0], (pixel_t *) iter->out[1],               \
+              (pixel_t *) iter->out[2], (pixel_t *) iter->out[3])
+
+#define REF_ENTRY(DUMMY, NAME, ...) &op_##NAME,
+#define DECL_ENTRY(SETUP, NAME, ...)                                            \
+    static const SwsOpEntry op_##NAME = {                                       \
+        .func = (SwsFuncPtr) NAME##_c,                                          \
+        __VA_ARGS__,                                                            \
+        SETUP                                                                   \
+    };
+
+#endif

quatric/mobipeg

libswscale/uops_backend.c
@@ -1,0 +1,197 @@
+/**
+ * Copyright (C) 2026 Niklas Haas
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include "libavutil/avassert.h"
+
+#include "uops_tmpl.h"
+
+/**
+ * We want to disable FP contraction because this is a reference backend that
+ * establishes a bit-exact reference result.
+ */
+#ifdef __clang__
+#pragma STDC FP_CONTRACT OFF
+#elif AV_GCC_VERSION_AT_LEAST(4, 8)
+#pragma GCC optimize ("fp-contract=off")
+#elif defined(_MSC_VER)
+#pragma fp_contract (off)
+#endif
+
+#if AV_GCC_VERSION_AT_LEAST(4, 4)
+#pragma GCC optimize ("finite-math-only")
+#endif
+
+/* Integer types */
+#define IS_FLOAT 0
+#  define BIT_DEPTH 8
+#    include "uops_tmpl.c"
+#  undef BIT_DEPTH
+#  define BIT_DEPTH 16
+#    include "uops_tmpl.c"
+#  undef BIT_DEPTH
+#  define BIT_DEPTH 32
+#    include "uops_tmpl.c"
+#  undef BIT_DEPTH
+#undef IS_FLOAT
+
+/* Floating point types */
+#define IS_FLOAT 1
+#  define BIT_DEPTH 32
+#    include "uops_tmpl.c"
+#  undef BIT_DEPTH
+#undef IS_FLOAT
+
+/* Expanded as new uop types are implemented in the C/template backend */
+#define REF_ALL_UOPS(TYPE)                                  \
+    SWS_FOR(TYPE, READ_PLANAR,    REF_ENTRY)                \
+    SWS_FOR(TYPE, READ_PLANAR_FV, REF_ENTRY)                \
+    SWS_FOR(TYPE, READ_PLANAR_FH, REF_ENTRY)                \
+    SWS_FOR(TYPE, READ_PACKED,    REF_ENTRY)                \
+    SWS_FOR(TYPE, READ_NIBBLE,    REF_ENTRY)                \
+    SWS_FOR(TYPE, READ_BIT,       REF_ENTRY)                \
+    SWS_FOR(TYPE, PERMUTE,        REF_ENTRY)                \
+    SWS_FOR(TYPE, COPY,           REF_ENTRY)                \
+    SWS_FOR(TYPE, WRITE_PLANAR,   REF_ENTRY)                \
+    SWS_FOR(TYPE, WRITE_PACKED,   REF_ENTRY)                \
+    SWS_FOR(TYPE, WRITE_NIBBLE,   REF_ENTRY)                \
+    SWS_FOR(TYPE, WRITE_BIT,      REF_ENTRY)                \
+    SWS_FOR(TYPE, SWAP_BYTES,     REF_ENTRY)                \
+    SWS_FOR(TYPE, EXPAND_BIT,     REF_ENTRY)                \
+    SWS_FOR(TYPE, EXPAND_PAIR,    REF_ENTRY)                \
+    SWS_FOR(TYPE, EXPAND_QUAD,    REF_ENTRY)                \
+    SWS_FOR(TYPE, TO_U8,          REF_ENTRY)                \
+    SWS_FOR(TYPE, TO_U16,         REF_ENTRY)                \
+    SWS_FOR(TYPE, TO_U32,         REF_ENTRY)                \
+    SWS_FOR(TYPE, TO_F32,         REF_ENTRY)                \
+    SWS_FOR(TYPE, SCALE,          REF_ENTRY)                \
+    SWS_FOR(TYPE, ADD,            REF_ENTRY)                \
+    SWS_FOR(TYPE, MIN,            REF_ENTRY)                \
+    SWS_FOR(TYPE, MAX,            REF_ENTRY)                \
+    SWS_FOR(TYPE, UNPACK,         REF_ENTRY)                \
+    SWS_FOR(TYPE, PACK,           REF_ENTRY)                \
+    SWS_FOR(TYPE, LSHIFT,         REF_ENTRY)                \
+    SWS_FOR(TYPE, RSHIFT,         REF_ENTRY)                \
+    SWS_FOR(TYPE, CLEAR,          REF_ENTRY)                \
+    SWS_FOR(TYPE, LINEAR,         REF_ENTRY)                \
+    SWS_FOR(TYPE, DITHER,         REF_ENTRY)                \
+    /* end of macro */
+
+static const SwsOpTable op_table = {
+    .block_size = SWS_BLOCK_SIZE,
+    .uops = true,
+    .entries = {
+        REF_ALL_UOPS(U8)
+        REF_ALL_UOPS(U16)
+        REF_ALL_UOPS(U32)
+        REF_ALL_UOPS(F32)
+        NULL
+    },
+};
+
+static void process(const SwsOpExec *exec, const void *priv,
+                    const int bx_start, const int y_start,
+                    int bx_end, int y_end)
+{
+    const SwsOpChain *chain = priv;
+    const SwsOpImpl *impl = chain->impl;
+    block_t x, y, z, w; /* allocate enough space for any intermediate */
+
+    SwsOpIter iterdata;
+    SwsOpIter *iter = &iterdata; /* for CONTINUE() macro to work */
+    iter->exec = exec;
+    for (int i = 0; i < 4; i++) {
+        iter->in[i]  = (uintptr_t) exec->in[i];
+        iter->out[i] = (uintptr_t) exec->out[i];
+    }
+
+    for (iter->y = y_start; iter->y < y_end; iter->y++) {
+        for (int block = bx_start; block < bx_end; block++) {
+            iter->x = block * SWS_BLOCK_SIZE;
+            CONTINUE(&x, &y, &z, &w);
+        }
+
+        const int y_bump = exec->in_bump_y ? exec->in_bump_y[iter->y] : 0;
+        for (int i = 0; i < 4; i++) {
+            iter->in[i]  += exec->in_bump[i] + y_bump * exec->in_stride[i];
+            iter->out[i] += exec->out_bump[i];
+        }
+    }
+}
+
+static int compile(SwsContext *ctx, SwsOpList *ops, SwsCompiledOp *out)
+{
+    int ret;
+
+    SwsOpChain *chain = ff_sws_op_chain_alloc();
+    if (!chain)
+        return AVERROR(ENOMEM);
+
+    SwsUOpList *uops = ff_sws_uop_list_alloc();
+    if (!uops) {
+        ret = AVERROR(ENOMEM);
+        goto fail;
+    }
+
+    ret = ff_sws_ops_translate(ops, uops);
+    if (ret < 0)
+        goto fail;
+
+    av_assert0(uops->num_ops > 0);
+    for (int i = 0; i < uops->num_ops; i++) {
+        const SwsOpTable *table = &op_table;
+        ret = ff_sws_uop_lookup(ctx, &table, 1, &uops->ops[i],
+                                SWS_BLOCK_SIZE, chain);
+        if (ret < 0)
+            goto fail;
+    }
+
+    *out = (SwsCompiledOp) {
+        .slice_align = 1,
+        .block_size  = SWS_BLOCK_SIZE,
+        .cpu_flags   = chain->cpu_flags,
+        .over_read   = chain->over_read,
+        .over_write  = chain->over_write,
+        .priv        = chain,
+        .free        = ff_sws_op_chain_free_cb,
+        .func        = process,
+    };
+
+    av_log(ctx, AV_LOG_DEBUG, "Compiled micro-ops:\n");
+    for (int i = 0; i < uops->num_ops; i++) {
+        char name[SWS_UOP_NAME_MAX];
+        ff_sws_uop_name(&uops->ops[i], name);
+        av_log(ctx, AV_LOG_DEBUG, "    %s\n", name);
+    }
+
+    ff_sws_uop_list_free(&uops);
+    return 0;
+
+fail:
+    ff_sws_uop_list_free(&uops);
+    ff_sws_op_chain_free(chain);
+    return ret;
+}
+
+const SwsOpBackend backend_uops = {
+    .name       = "uops",
+    .flags      = SWS_BACKEND_C,
+    .compile    = compile,
+    .hw_format  = AV_PIX_FMT_NONE,
+};

libswscale/uops_tmpl.h
@@ -1,0 +1,146 @@
+/**
+ * Copyright (C) 2026 Niklas Haas
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef SWSCALE_UOPS_TMPL_H
+#define SWSCALE_UOPS_TMPL_H
+
+/**
+ * Helper macros for the C-based backend.
+ *
+ * To use these macros, `pixel_t` should be defined as the type of pixels.
+ */
+
+#include <assert.h>
+#include <float.h>
+#include <stdint.h>
+
+#include "libavutil/attributes.h"
+
+#include "ops_chain.h"
+#include "uops_macros.h"
+
+#ifndef SWS_BLOCK_SIZE
+#  define SWS_BLOCK_SIZE 32
+#endif
+
+typedef union block_t {
+    uint8_t   u8[SWS_BLOCK_SIZE];
+    uint16_t u16[SWS_BLOCK_SIZE];
+    uint32_t u32[SWS_BLOCK_SIZE];
+    float    f32[SWS_BLOCK_SIZE];
+} block_t;
+
+#define SIZEOF_BLOCK (sizeof(pixel_t) * SWS_BLOCK_SIZE)
+
+/**
+ * Internal context holding per-iter execution data. The data pointers will be
+ * directly incremented by the corresponding read/write functions.
+ */
+typedef struct SwsOpIter {
+    uintptr_t in[4];
+    uintptr_t out[4];
+    int x, y;
+
+    /* Link back to per-slice execution context */
+    const SwsOpExec *exec;
+} SwsOpIter;
+
+#ifdef __clang__
+#  define SWS_LOOP AV_PRAGMA(clang loop vectorize(assume_safety))
+#elif defined(__GNUC__)
+#  define SWS_LOOP AV_PRAGMA(GCC ivdep)
+#else
+#  define SWS_LOOP
+#endif
+
+/* Miscellaneous helpers */
+#define bitfn2(name, ext) name ## _ ## ext
+#define bitfn(name, ext)  bitfn2(name, ext)
+#define fn(name)          bitfn(name, PX)
+
+#define bump_ptr(ptr, bump) ((pixel_t *) ((uintptr_t) (ptr) + (bump)))
+
+/* Helpers for dealing with component masks */
+#define X SWS_COMP_TEST(mask, 0)
+#define Y SWS_COMP_TEST(mask, 1)
+#define Z SWS_COMP_TEST(mask, 2)
+#define W SWS_COMP_TEST(mask, 3)
+
+/* Helper macros to make writing common function signatures less painful */
+#define DECL_FUNC(NAME, ...)                                                    \
+    static av_always_inline void                                                \
+        fn(NAME)(SwsOpIter *restrict iter, const SwsOpImpl *restrict impl,      \
+                 pixel_t *restrict x, pixel_t *restrict y,                      \
+                 pixel_t *restrict z, pixel_t *restrict w,                      \
+                 __VA_ARGS__)
+
+#define DECL_READ(NAME, ...)                                                    \
+    DECL_FUNC(NAME, __VA_ARGS__,                                                \
+              const pixel_t *restrict in0, const pixel_t *restrict in1,         \
+              const pixel_t *restrict in2, const pixel_t *restrict in3)         \
+
+#define DECL_WRITE(NAME, ...)                                                   \
+    DECL_FUNC(NAME, __VA_ARGS__,                                                \
+              pixel_t *restrict out0, pixel_t *restrict out1,                   \
+              pixel_t *restrict out2, pixel_t *restrict out3)                   \
+
+#define CALL(NAME, ...) fn(NAME)(iter, impl, x, y, z, w, __VA_ARGS__)
+
+/* Helper macro to call into the next continuation with a given type */
+#define CONTINUE(...)                                                           \
+    ((void (*)(SwsOpIter *, const SwsOpImpl *,                                  \
+               void *restrict x, void *restrict y,                              \
+               void *restrict z, void *restrict w)) impl->cont)                 \
+        (iter, &impl[1], __VA_ARGS__)
+
+/* Helper macros for common op setup code */
+#define DECL_SETUP(NAME, PARAMS, OUT)                                           \
+    static av_unused int fn(NAME)(const SwsImplParams *PARAMS,                  \
+                                  SwsImplResult *OUT)
+
+/* Helper macro for declaring kernel entry points */
+#define DECL_IMPL(FUNC, NAME, TYPE, UOP, ...)                                   \
+    static av_flatten void NAME##_c(SwsOpIter *restrict iter,                   \
+                                    const SwsOpImpl *restrict impl,             \
+                                    void *restrict x, void *restrict y,         \
+                                    void *restrict z, void *restrict w)         \
+    {                                                                           \
+        CALL(FUNC, __VA_ARGS__);                                                \
+    }
+
+#define DECL_IMPL_READ(...)                                                     \
+    DECL_IMPL(__VA_ARGS__,                                                      \
+              (const pixel_t *) iter->in[0], (const pixel_t *) iter->in[1],     \
+              (const pixel_t *) iter->in[2], (const pixel_t *) iter->in[3])
+
+#define DECL_IMPL_WRITE(...)                                                    \
+    DECL_IMPL(__VA_ARGS__,                                                      \
+              (pixel_t *) iter->out[0], (pixel_t *) iter->out[1],               \
+              (pixel_t *) iter->out[2], (pixel_t *) iter->out[3])
+
+#define REF_ENTRY(DUMMY, NAME, ...) &op_##NAME,
+#define DECL_ENTRY(SETUP, NAME, ...)                                            \
+    static const SwsOpEntry op_##NAME = {                                       \
+        .func = (SwsFuncPtr) NAME##_c,                                          \
+        __VA_ARGS__,                                                            \
+        SETUP                                                                   \
+    };
+
+#endif